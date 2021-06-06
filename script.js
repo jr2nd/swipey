59a63fb1d424bc2ea8d427b4253c02f782c200ee
jr2nd <junior2nd@gmail.com>
2021-06-05 20:42:40 -0500

    3 files modified

--- script.js
+++ script.js
@@ -2,34 +2,25 @@ let swipeycontainer = document.getElementById('swipey-container');
 let swipey = document.getElementsByClassName('swipey');
 let coords = document.getElementById('coords');
 let down = false;
-let mywidth = 300;
 
 for (let i = 0; i < swipey.length; i++) {
   swipey[i].addEventListener('mousedown', swipeon);
   swipey[i].addEventListener('mouseup', swipeoff);
   swipey[i].addEventListener('mousemove', swipemove);
   swipey[i].addEventListener('mouseout', swipeout);
-  swipey[i].style.width = mywidth + 'px';
 }
 
 function swipeon() {
   down = true;
 }
 
-function swipeoff() {
-  this.style.width="300px"
-  down = false;
-}
+function swipeoff() {}
 
 function swipemove() {
   if (down === true) {
-    mywidth -= 1;
-    this.style.width = mywidth + 'px';
-    alert('move')
   } //if
 } //function
 
 function swipeout() {
-  this.style.width = "300px";
   down = false;
 }
