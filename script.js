1fcd0e6912e0ada039ff7ffadb40ae52f6836db0
jr2nd <junior2nd@gmail.com>
2021-06-24 21:55:23 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const swipeycontainer = document.querySelector('#swipey-container');
-const swipeys = document.querySelector('.swipey');
+const swipey = document.querySelector('.swipey');
 const answer = document.querySelector('#answer');
 const startstop = document.querySelector('#startstop');
 const coords = document.querySelector('#coords');
 const mousex = document.querySelector('#mousex');
-const mousey = cocument.querySelector('#mousey');
+const mousey = document.querySelector('#mousey');
 
 for (let i = 0; i < swipey.length; i++) {
   swipey[i].addEventListener('touchstart', swipestart);
