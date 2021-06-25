bb93f60d62732ce37f3306771b5393ac8af6fe8c
jr2nd <junior2nd@gmail.com>
2021-06-24 21:53:15 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-let swipeycontainer = document.getElementById('swipey-container');
-let swipey = document.getElementsByClassName('swipey');
-let coords = document.getElementById('coords');
-let down = false;
+const swipeycontainer = document.querySelector('#swipey-container');
+const swipeys = document.querySelector('.swipey');
+const answer = document.querySelector('#answer');
+const startstop = document.querySelector('#startstop');
+const coords = document.querySelector('#coords');
+const mousex = document.querySelector('#mousex');
+const mousey = cocument.querySelector('#mousey');
 
 for (let i = 0; i < swipey.length; i++) {
-  swipey[i].addEventListener('touchstart', swipemove);
-  swipey[i].addEventListener('touchmove', swipeout);
-  swipey[i].addEventListener('touchcancel', swipemove);
-  swipey[i].addEventListener('touchcancel', logswipe);
+  swipey[i].addEventListener('touchstart', swipestart);
+  swipey[i].addEventListener('touchmove', swipemove);
+  swipey[i].addEventListener('touchcancel', swipecancel);
+  swipey[i].addEventListener('touchend', swipeend);
 }
 
-function logswipe() {
-  console.log('logswipe');
+function swipestart() {
+  answer.innerHTML = "swipe start"
 }
-
-function swipeon() {
-  down = true;
-}
-
-function swipeoff() {}
-
 function swipemove() {
-  if (down === true) {
-  } //if
-} //function
 
-function swipeout() {
-  down = false;
 }
-
-function touchswipe() {
-  alert('touchswipe');
+function swipecancel() {
+  startstop.innerHTML = 'swipe cancelled'
+}
+function swipeend() {
+  startstop.innerHTML = 'swipe end'
 }
