e05a92b4f90531735f1cc68c4c212176926df146
jr2nd <junior2nd@gmail.com>
2021-06-27 15:34:06 -0500

    1 file modified

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ slider < 25 ? color1 = 'yellow' : color1 = 'black';
 slider < 50 ? color2 = 'yellow' : color2 = 'black';
 slider < 75 ? color3 = 'yellow' : color3 = 'black';
 slider < 100 ? color4 = 'yellow' : color4 = 'black';
+startstop.innerHTML = color1 + color2 + color3 + color4;
 this.style.backgroundImage = "linear-gradient( to left, " +
 color1 + ", " +
 color2 + ", " +
