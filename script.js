3b773eb0b51e06ed2d473d8574711a2a7b4a5af9
jr2nd <junior2nd@gmail.com>
2021-06-27 14:08:14 -0500

    1 file modified

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ for (let i = 0; i < swipey.length; i++) {
 }
 
 function swipestart(e) {
-  this.style.color='blue';
+  this.style.color='blue' ? this.style.color = 'red' : this.style.color='blue';
 }
 function swipemove(e) {
 this.style.backgroundColor='purple'
