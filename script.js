a4466f7e88702ea701e8df907f502e4199a43fb5
jr2nd <junior2nd@gmail.com>
2021-06-27 23:55:35 -0500

    1 file modified

--- script.js
+++ script.js
@@ -8,28 +8,34 @@ const mousey = document.querySelector('#mousey');
 
 for (let i = 0; i < swipey.length; i++) {
   swipey[i].addEventListener('touchstart', swipestart);
-  swipey[i].addEventListener('touchmove', swipemove);
+ // swipey[i].addEventListener('touchmove', swipemove);
   swipey[i].addEventListener('touchcancel', swipecancel);
   swipey[i].addEventListener('touchend', swipeend);
 }
 function swipestart(e) {
-let swipeleft = e.touches[0].clientX > swipey[0].clientWidth / 2;
-startstop.innerHTML = swipeleft;
+  let noswipe = e.touches[0].clientX > swipey[0].clientWidth / 4 &&
+                e.touches[0].clientX < (swipey[0].clientWidth / 4) * 3;
+  !noswipe ? swipemove() : e.innerHTML = 'nope';
 }
 function swipemove(e) {
-let swipeyX = e.touches[0].clientX;
-let swipeywidth = swipey[0].clientWidth;
-let color1, color2, color3, color4;
-slider = parseInt((swipeyX / swipeywidth) * 100);
-slider < 25 ? color1 = 'yellow' : color1 = 'black';
-slider < 50 ? color2 = 'yellow' : color2 = 'black';
-slider < 75 ? color3 = 'yellow' : color3 = 'black';
-slider < 100 ? color4 = 'yellow' : color4 = 'black';
-this.style.backgroundImage = "linear-gradient( to left, " +
-color1 + ", " +
-color2 + ", " +
-color3 + ", " +
-color4 + ")";
+  let swipeyX = e.touches[0].clientX;
+  let swipeywidth = swipey[0].clientWidth;
+  let color1, color2, color3, color4;
+  slider = parseInt((swipeyX / swipeywidth) * 100);
+  slider < 25 ? (color1 = 'yellow') : (color1 = 'black');
+  slider < 50 ? (color2 = 'yellow') : (color2 = 'black');
+  slider < 75 ? (color3 = 'yellow') : (color3 = 'black');
+  slider < 100 ? (color4 = 'yellow') : (color4 = 'black');
+  this.style.backgroundImage =
+    'linear-gradient( to left, ' +
+    color1 +
+    ', ' +
+    color2 +
+    ', ' +
+    color3 +
+    ', ' +
+    color4 +
+    ')';
 }
 function swipecancel(e) {
   startstop.innerHTML = 'swipe cancelled';
