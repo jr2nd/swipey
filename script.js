53440d06c41f18769e002998d1fc9a975364c2a3
jr2nd <junior2nd@gmail.com>
2021-07-01 10:48:41 -0500

    2 files modified

--- script.js
+++ script.js
@@ -8,24 +8,23 @@ const mousey = document.querySelector('#mousey');
 
 for (let i = 0; i < swipey.length; i++) {
   swipey[i].addEventListener('touchstart', swipestart);
- swipey[i].addEventListener('touchmove', swipemove);
+  swipey[i].addEventListener('touchmove', swipemove);
   swipey[i].addEventListener('touchcancel', swipecancel);
   swipey[i].addEventListener('touchend', swipeend);
-}
+} //for
 function swipestart(event) {
   let swiperight = event.touches[0].clientX < swipey[0].clientWidth / 4;
   let swipeleft = event.touches[0].clientX > (swipey[0].clientWidth / 4) * 3;
   if (swiperight) swipemove(event, this, 'swiperight');
   if (swipeleft) swipemove(event, this, 'swipeleft');
-}
+} //swipestart()
 function swipemove(e, t, leftOrRight) {
-  startstop.innerHTML = leftOrRight;//DEBUG
+  startstop.innerHTML = leftOrRight; //DEBUG
   let swipeyX = e.touches[0].clientX;
   let swipeywidth = swipey[0].clientWidth;
   let swipeyDirection = 'to right';
-  //leftOrRight === 'swiperight' ? color1 = 'black' : color1 = 'yellow';  leftOrRight === 'swipeleft' ? color4 = 'black' : color4 = 'yellow';
-  if(leftOrRight === 'swipeleft')swipeyDirection == 'to left';
-  if(leftOrRight === 'swiperight')swipeyDirection = 'to right';
+  if (leftOrRight === 'swipeleft') swipeyDirection == 'to left';
+  if (leftOrRight === 'swiperight') swipeyDirection = 'to right';
   slider = parseInt((swipeyX / swipeywidth) * 100);
   let color1 = 'black',
     color2,
@@ -34,11 +33,11 @@ function swipemove(e, t, leftOrRight) {
   if (swipeyDirection === 'to left') {
     slider < 75 ? (color2 = 'black') : (color2 = 'yellow');
     slider < 50 ? (color3 = 'black') : (color3 = 'yellow');
-  }
+  } //if
   if (swipeyDirection === 'to right') {
     slider > 25 ? (color2 = 'black') : (color2 = 'yellow');
     slider > 50 ? (color3 = 'black') : (color3 = 'yellow');
-  }
+  } //if
   t.style.backgroundImage =
     'linear-gradient(' +
     swipeyDirection +
@@ -51,10 +50,10 @@ function swipemove(e, t, leftOrRight) {
     ', ' +
     color4 +
     ')';
-  }
+} //swipemove()
 function swipecancel(e) {
   startstop.innerHTML = 'swipe cancelled';
-}
+} //swipecancel()
 function swipeend(e) {
   startstop.innerHTML = 'swipe end';
-}
+} //swipeend()
