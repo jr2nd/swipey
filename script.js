558bcb7128144a6c4765dc6134314ed044d8a8e7
jr2nd <junior2nd@gmail.com>
2021-07-04 14:50:37 -0500

    1 file modified

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ for (let i = 0; i < swipey.length; i++) {
   swipey[i].addEventListener('touchend', swipeend);
 } //add touch event listeners`
 function swipestart() {
+  console.log('swipestart: ' + this.clientX)//DEBUG
   this.clientX < swipey[0].clientWidth / 4
     ? (swipeyDirection = 'to right')
     : (swipeyDirection = 'to left');
