c80fa35a5d2e37a0f9f417cab204e035c1c4c229
jr2nd <junior2nd@gmail.com>
2021-07-04 14:17:18 -0500

    1 file modified

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function swipestart() {
 swipemove(this, swipeyDirection);
 } //swipestart()
 function swipemove(t, leftOrRight) {
+  console.log('first: ' + leftOrRight)
 /*  let swipeyX = this.event.touches[0].clientX;
   let swipeyWidth = swipey[0].clientWidth;*/
  // slider = parseInt((swipeyX / swipeyWidth) * 100);
