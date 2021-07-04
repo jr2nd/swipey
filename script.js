243197ab22a5e6af6d951e0a1466251385470685
jr2nd <junior2nd@gmail.com>
2021-07-04 14:14:09 -0500

    1 file modified

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function swipestart() {
 swipemove(this, swipeyDirection);
 } //swipestart()
 function swipemove(t, leftOrRight) {
-  let swipeyX = Event.touches[0].clientX;
+  let swipeyX = this.event.touches[0].clientX;
   let swipeyWidth = swipey[0].clientWidth;
   slider = parseInt((swipeyX / swipeyWidth) * 100);
   let color1 = 'black',
